ea17938a71627b8ed85f73f51ed3b3c0ffddf21b
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-11-07 11:11:24 +0100

    polish

--- src/at/mus/demo/tests/CameraTest.java
+++ src/at/mus/demo/tests/CameraTest.java
@@ -2,13 +2,21 @@ package at.mus.demo.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import at.mus.demo.SDCard;
 
 public class CameraTest {
 
+    SDCard one = new SDCard(1000);
+
+    @BeforeEach
     @Test
-    void testGetCreationDate() {
-        assertEquals(1, 1);
+    void testTakePhoto() {
+
+        assertEquals(one.getFiles().get(0).getSize(), 0);
+
     }
 
 }
--- src/at/mus/demo/tests/SDCardTest.java
+++ src/at/mus/demo/tests/SDCardTest.java
@@ -7,28 +7,24 @@ import org.junit.jupiter.api.Test;
 import at.mus.demo.SDCard;
 
 public class SDCardTest {
-    @Test
-    void testTakePhoto() {
-        SDCard one = new SDCard(1000);
-        assertEquals(one.getFiles().get(0).getSize(), 0);
 
-    }
+    SDCard one = new SDCard(1000);
 
     @Test
     void testGetCapacity() {
-        SDCard one = new SDCard(1000);
+
         assertEquals(one.getCapacity(), 1000);
     }
 
     @Test
     void testGetFiles() {
-        SDCard one = new SDCard(1000);
+
         assertEquals(one.getFiles().size(), 0);
     }
 
     @Test
     void testGetFreeSpace() {
-        SDCard one = new SDCard(1000);
+
         assertEquals(one.getFreeSpace(), 1000);
     }
 }
