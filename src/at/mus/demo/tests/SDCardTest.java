bc87be93ef1822aaf484e4c7f461aa0f8daf28d1
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-11-07 10:36:15 +0100

    SDCard Tests

--- src/at/mus/demo/tests/SDCardTest.java
+++ src/at/mus/demo/tests/SDCardTest.java
@@ -6,24 +6,34 @@ import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
+import at.mus.demo.SDCard;
+
 public class SDCardTest {
     @Test
     void testTakePhoto() {
+        SDCard one = new SDCard(1000);
+        one.TakePhoto();
+        assertEquals(one.getFiles().get(0).getSize(), 300);
 
     }
 
     @Test
     void testGetCapacity() {
-
+        SDCard one = new SDCard(1000);
+        assertEquals(one.getCapacity(), 1000);
     }
 
     @Test
     void testGetFiles() {
-
+        SDCard one = new SDCard(1000);
+        one.TakePhoto();
+        assertEquals(one.getFiles().size(), 1);
     }
 
     @Test
     void testGetFreeSpace() {
-
+        SDCard one = new SDCard(1000);
+        one.TakePhoto();
+        assertEquals(one.getFreeSpace(), 700);
     }
 }
