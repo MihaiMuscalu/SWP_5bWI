4cdfb0c1030b5a88098f65f82c23026562f120d9
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-11-07 10:22:02 +0100

    SdCard

--- src/at/mus/demo/tests/FileTest.java
+++ src/at/mus/demo/tests/FileTest.java
@@ -1,25 +1,27 @@
 package at.mus.demo.tests;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class FileTest {
     @Test
     void testGetCreationDate() {
-
+        assertEquals(1, 1);
     }
 
     @Test
     void testGetName() {
-
+        assertEquals("a", "a");
     }
 
     @Test
     void testGetSize() {
-
+        assertEquals(1, 1);
     }
 
     @Test
     void testSetName() {
-
+        assertEquals("a", "a");
     }
 }
---
+++ src/at/mus/demo/tests/SDCardTest.java
@@ -0,0 +1,29 @@
+package at.mus.demo.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+public class SDCardTest {
+    @Test
+    void testTakePhoto() {
+
+    }
+
+    @Test
+    void testGetCapacity() {
+
+    }
+
+    @Test
+    void testGetFiles() {
+
+    }
+
+    @Test
+    void testGetFreeSpace() {
+
+    }
+}
